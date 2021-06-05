0b15028b26d87cbde55f5eb0c357831be1335e3f
Zhimao Lin <zhimao.lin@thoughtworks.com>
2021-06-05 10:03:14 +0800

    [zhimao] fill form router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { ReactiveFormComponent } from './form/reactive-form/reactive-form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToDoListModule } from './to-do-list/to-do-list.module';
 import { HomePageComponent } from './home-page/home-page.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { ServiceBService } from './module-a/services/service-b.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
+import { TemplateDrivenFormComponent } from './form/template-driven-form/template-driven-form.component';
 import { FormModule } from './form/form.module';
 import { SharedModule } from './shared/shared.module';
 
--- src/app/form/form.router.ts
+++ src/app/form/form.router.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormComponent } from './form.component';
-import {FormItemComponent} from './formItem/form-item.component'
+import { FormItemComponent} from './formItem/form-item.component';
 import { NgGroupComponent } from './ng-group/ng-group.component';
 import { AppComponent } from './dynamic-form/app.component';
+import { NgFormBuilderComponent } from './ng-form-builder/ng-form-builder.component';
+import { CustomValidatorComponent } from './custom-validator/custom-validator.component';
+import { CustomValidator2Component } from './custom-validator2/custom-validator2.component';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
 const routes: Routes = [
   {
@@ -15,8 +20,28 @@ const routes: Routes = [
     component: FormItemComponent,
   },
   {
+    path: 'template-driven-form',
+    component: TemplateDrivenFormComponent,
+  },
+  {
+    path: 'reactive-form',
+    component: ReactiveFormComponent,
+  },
+  {
     path: 'group',
     component: NgGroupComponent,
+  },
+  {
+    path: 'builder',
+    component: NgFormBuilderComponent,
+  },
+  {
+    path: 'validator',
+    component: CustomValidatorComponent,
+  },
+  {
+    path: 'validator2',
+    component: CustomValidator2Component,
   },
   {
     path: 'dynamic',
