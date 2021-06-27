31974761c609dc9c52dad480681c959b7e7a853a
forcheng <chuang@thoughtworks.com>
2021-06-27 23:35:35 +0800

    [huang cheng] enable debug tools

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
-import { enableProdMode } from '@angular/core';
+import { ApplicationRef, enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { enableDebugTools } from '@angular/platform-browser';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule).then(moduleRef => {
+  const applicationRef = moduleRef.injector.get(ApplicationRef);
+  const applicationComponent = applicationRef.components[0];
+  enableDebugTools(applicationComponent);
+}).catch(err => console.error(err));
