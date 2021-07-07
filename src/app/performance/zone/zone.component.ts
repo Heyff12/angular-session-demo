86976349214563e577cad328775bb51bf3dc33f3
forcheng <chuang@thoughtworks.com>
2021-07-07 23:00:18 +0800

    [huang cheng] update zonejs demo

--- src/app/performance/zone/zone.component.ts
+++ src/app/performance/zone/zone.component.ts
@@ -2,25 +2,29 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-zone-component',
-  templateUrl: './zone.component.html',
+  template: `<button (click)='onClick()'>Run logZone</button>`
 })
 export class ZoneComponent implements OnInit {
   logZone: Zone;
 
   ngOnInit(): void {
+    // 创建一个名为 logZone 的 Zone
     this.logZone = Zone.current.fork({
       name: 'logZone',
+      // 当回调被调用时，会由该钩子函数接管回调的触发过程
       onInvoke: (parentZoneDelegate, currentZone, targetZone, delegate, applyThis, applyArgs, source) => {
         console.log(targetZone.name, 'enter');
+        // 执行回调
         parentZoneDelegate.invoke(targetZone, delegate, applyThis, applyArgs, source);
         console.log(targetZone.name, 'leave'); }
     });
   }
 
-  onRunLogZoneClick(): void {
-    this.logZone.run(() => {
+  onClick(): void {
+    this.logZone.run(() => { // 第一次回调
       console.log(Zone.current.name, 'queue promise');
-      Promise.resolve('OK').then((value) => {
+
+      Promise.resolve('OK').then((value) => { // 第二次回调
         console.log(Zone.current.name, 'Promise', value);
       });
     });
